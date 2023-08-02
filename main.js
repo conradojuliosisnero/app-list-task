596fc7c6597cd12776838e8ca4e4cd65e6d01e23
conrado julio <conradojuliosisnero@gmail.com>
2023-08-02 12:51:17 -0500

    agregando funciones de botones

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-const input = document.querySelector('.input');
-const button = document.querySelector('.btn__task');
-const list = document.querySelector('.list__task');
-const task = document.querySelector('task');
+    // ELEMENTOS    
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn__task");
+const list = document.querySelector(".list__task");
+const tasks = document.querySelector(".task");
+const btndelete = document.querySelector('.btn__detele-task');
 
+    // funcion AGREGAR TAREAS 
 
-    function addTask() {
-        // let li = document.createElement('li')
-        // li.appendChild(document.createTextNode(input.value))
-        // console.log(input.value)
-        // task.appendChild(li)
+    // function addTask() {
+    //     // let li = document.createElement('li')
+    //     // li.appendChild(document.createTextNode(input.value))
+    //     // console.log(input.value)
+    //     // task.appendChild(li)
 
-        // Capturamos el valor del input
-        let inputValue = input.value;
-        console.log(inputValue)
-        // creamos un nuevo elemento li
-        const newElement = document.createElement('li');
+    //     // Capturamos el valor del input
+    //     let inputValue = input.value;
+    //     console.log(inputValue)
+    //     // creamos un nuevo elemento li
+    //     const newElement = document.createElement('li');
         
-        // newElement.textContent = inputValue.textContent;
+    //     // newElement.textContent = inputValue.textContent;
+    // }
+
+    //  funcion SELECCIONAR ELEMENTOS 
+    function select() {
+       tasks.classList.toggle('select');
+       tasks.classList.toggle('completed')
     }
 
-    function pressEnter() {
+    // funcion BORRAR TAREA
+    function deteleTask() {
+        tasks.remove();
     }
 
-    button.addEventListener('click',addTask)
-    input.addEventListener('keypress',pressEnter)
+    // listeners     
+    tasks.addEventListener('click',select);
+    btndelete.addEventListener('click',deteleTask);
+
+    // button.addEventListener('click',addTask)
