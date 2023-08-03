1d578fa9d53ee873eeb1b5a8adcaae55fcf35fba
conrado julio <conradojuliosisnero@gmail.com>
2023-08-03 12:20:27 -0500

    funcion de agregar y eliminar tareas agregadas

--- Js/main.js
+++ Js/main.js
@@ -5,60 +5,54 @@ const list = document.querySelector(".list__task");
 const taskCompleted = document.querySelector(".list__task__completed");
 const tasks = document.querySelectorAll(".task");
 const leyenda = document.querySelector(".leyend");
-const btndelete = document.querySelector(".btn__detele-task");
 const texto = document.querySelector(".texto");
-
 
 //variables
 let idCounter = 0;
-idCounter++;
-let valueInput = input.value;
 
 // funcion AGREGAR TAREA
 function addTask() {
     if (input.value !== "") {
+        
+        idCounter++;
         let Input = input.value;
-        list.innerHTML += `<li class="task" id="${idCounter}">
+        list.innerHTML += `<li class="task" id="task${idCounter}">
                 <p>${Input}</p>
-                <button class="btn__detele-task active">X</button>
+                <button class="detele-task active" id="detele-task${idCounter}">X</button>
             </li>`;
         input.value = "";
     }
 }
 
+button.addEventListener("click", addTask);
 
 // funcion BORRAR TAREA
-function deteleTask() {
-    // if (btndelete.classList.contains('active')) {
-        
-    // }
-
+function deteleTask(id) {
+    let taskRemove = document.getElementById(id);
+    taskRemove.remove();
 }
 
-tasks.forEach(()=>{
-    btndelete.addEventListener('click',deteleTask);
-
+list.addEventListener('click',(event)=>{
+    if (event.target.className === 'detele-task active') {
+        const id = event.target.id.replace('detele-task','task');
+        deteleTask(id);
+    }
 })
 
 
 //  funcion SELECCIONAR ELEMENTOS
-// function selection() {
-//     // agregamos estas clases
-// 	tasks.classList.toggle("select");
-// 	tasks.classList.toggle("completed");
-//     if (tasks.classList.contains('select')) {
-//         let tarea = taskCompleted.innerHTML +=
-//         `<li class="task completed select" id="${idCounter}">
-//             <p>${texto.textContent}</p>
-//         </li>`;
-//     }else{
-//         tarea.remove()
-//     }
-// }
-//iteremos sobre todas las tareas
-// tasks.forEach( ()=>{
-//     tasks.addEventListener("click", selection);
-// })
+function selection() {
+    // agregamos estas clases
+	tasks.classList.toggle("select");
+	tasks.classList.toggle("completed");
+    if (tasks.classList.contains('select')) {
+        let tarea = taskCompleted.innerHTML +=
+        `<li class="task completed select" id="${idCounter}">
+            <p>${texto.textContent}</p>
+        </li>`;
+    }else{
+        tarea.remove()
+    }
+}
 
-// LISTENERS
-button.addEventListener("click", addTask);
+
